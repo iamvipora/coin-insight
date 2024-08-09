3a11260f7d75f589993de47dc9d8c4685c4f3e1c
iamvipora <henrydizon99@gmail.com>
2024-08-09 20:52:10 +0800

    adjust h2 text styling

--- src/components/FeaturedCoins.jsx
+++ src/components/FeaturedCoins.jsx
@@ -4,8 +4,8 @@ import { FaFire } from "react-icons/fa6"
 
 function FeaturedCoins() {
   return (
-    <div className='flex flex-col px-2 pb-4 w-full bg-[#EFF2F5] rounded-lg'>
-      <h2 className='flex items-center gap-1 text-lg font-bold py-2'>{<FaFire className='text-red-600'/>} Trending Coins</h2>
+    <div className='flex flex-col p-2 w-full bg-[#EFF2F5] rounded-lg'>
+      <h2 className='flex items-center gap-1 text-lg font-bold pb-2'>{<FaFire className='text-red-600'/>} Trending Coins</h2>
       <div className='flex flex-col gap-2'>
         {/* MAP COINS HERE */}
         <CoinBox/>
